3d903ac3730efba9fc11b464b865937dab7c7294
Ashutosh Kumar <ashu.iitkgp@gmail.com>
2018-08-20 12:14:54 +0530

    Add files via upload

---
+++ CNN/text_1DCNN/01_1D_CNN_imdb_classification.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 17 10:35:19 2018
+
+@author: ashutosh
+
+IMDB text classification using 1D CNN
+
+"""
+
+import numpy as np
+import pandas as pd
+from keras.datasets import imdb
+
